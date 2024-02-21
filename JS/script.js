3b5ae7f4a88a35d4c096cc7b49a3c5f07c6e240d
NicolaBellini <bellininicola4366@gmail.com>
2024-02-21 14:35:08 +0100

    [add] total price and relative discount plus input const

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-console.log("ciao");
+// ----------INPUT-----------
+const inputName = document.getElementById("nome").value
+const inputKm = document.getElementById("km").value
+
+
+//--------- ELEMENT----------
+const name = inputName
+const trip = inputKm
+const kmPrice = 0.21
+const price = parseFloat ((trip*kmPrice).toFixed(2))
+const oldPrice = parseFloat ((price*0.6).toFixed(2))
+const babyPrice = parseFloat ((price*0.8).toFixed(2))
+
+
+console.log(oldPrice);
