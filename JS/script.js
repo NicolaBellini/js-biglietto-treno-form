6f660eca833f20be942c039d5eaf9a6e80eb5c83
NicolaBellini <bellininicola4366@gmail.com>
2024-02-21 16:54:43 +0100

    [add] ticket and .innerhtml info

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,34 @@
 
 let form = document.getElementById("ticketForm")
+let cost = document.getElementById("ticketPrice")
+let offerType = document.getElementById("offertype")
+let nameOnTicket = document.getElementById("ticket-name")
 
 function priceMaker() {
   const inputName = document.getElementById("name").value;
   const inputKm = document.getElementById("km").value;
   const age = document.getElementById("ageUser").value;
+
   
   const trip = parseFloat(inputKm);
   const kmPrice = 0.21;
   let price = parseFloat((trip * kmPrice).toFixed(2));
   
-  if (age === "baby") {
+  if (age === "Young") {
     price *= 0.8;
-  } else if (age === "old") {
+  } else if (age === "Senior") {
     price *= 0.6;
   }
   
   console.log("Il prezzo del viaggio è:", price.toFixed(2));
 
+  nameOnTicket.innerHTML = inputName
+  offerType.innerHTML = age
+  cost.innerHTML = price.toFixed(2) + " euro"
+
   form.reset();
 }
+
 
 // svolgo la funzione priceMaker e resetto il form
 
